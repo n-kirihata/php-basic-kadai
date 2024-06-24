fcde1e9acf6a99e2edaf6709746965dc79afdded
n-kirihata <163854084+n-kirihata@users.noreply.github.com>
2024-06-24 19:57:39 +0900

    「kadai_011」の提出

---
+++ kadai_011/foreach.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP_kadai_011</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $onions = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+
+    foreach ($onions as $key => $value) {
+      echo "{$key} : {$value}<br>";
+    }
+    ?>
+  </p>
+  
+</body>
+
+</html>
+
+
