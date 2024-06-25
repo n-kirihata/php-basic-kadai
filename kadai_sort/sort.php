5ce302204c7dbbd5390ed074800930f4d2d00036
n-kirihata <163854084+n-kirihata@users.noreply.github.com>
2024-06-25 20:46:35 +0900

    「kadai_sort」の提出

---
+++ kadai_sort/sort.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10];
+        
+        function sort_2way($array, $order) {
+            if ($order) { // TRUEであれば昇順にソートsort
+            echo '昇順にソートします。<br>';
+            sort($array);
+            } else { // FALSEであれば降順にソートrsort
+            echo '降順にソートします。<br>';
+            rsort($array);
+            }
+            foreach ($array as $num) {
+              echo $num . '<br>';
+            }
+        }
+        sort_2way($nums, true);
+        sort_2way($nums, false);
+      ?>
+    </p>
+</body>
+
+</html>
+
